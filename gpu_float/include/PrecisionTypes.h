8e7aada5c06efb62b6530faab40275d0acd79117
zehaoj <zehao323@gmail.com>
2023-01-08 00:38:31 +0100

    cuda half implemented

---
+++ gpu_float/include/PrecisionTypes.h
@@ -0,0 +1,14 @@
+#include <cuda_fp16.h>
+
+
+#ifndef PRECISIONTYPES_H
+#define PRECISIONTYPES_H
+
+// precision for particle quanties (position, velocity, ...): choose float or double
+typedef float FPpart;
+// precision for field quantities (Ex, Bx, ...): choose float or double
+typedef float FPfield;
+// precision for interpolated quanties (Ex, Bx, ...): choose float or double
+typedef float FPinterp;
+
+#endif
